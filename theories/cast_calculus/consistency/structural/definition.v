91fa7c905b80779e28a0dce8275d50a5335a96c2
Koen Jacobs <koen.pub@pm.me>
2020-02-21 08:58:38 +0100

    let's try will real unfolding of types instead!

--- theories/cast_calculus/consistency/structural/definition.v
+++ theories/cast_calculus/consistency/structural/definition.v
@@ -4,108 +4,101 @@ From fae_gtlc_mu.cast_calculus.consistency.structural Require Export assumption.
 From fae_gtlc_mu Require Import prelude.
 Require Coq.Logic.JMeq.
 
-Reserved Notation "A ⊢ τ ~ τ'" (at level 4, τ, τ' at next level).
-Inductive cons_struct (A : list Assumption) : type -> type -> Type :=
-  (** ground and star *)
-  (* downcasting from star to ground *)
-  | consStarTGround τ :
-      Ground τ →
-      A ⊢ ⋆ ~ τ
-  (* upcasting from ground to star *)
-  | consTGroundStar τ :
-      Ground τ →
-      A ⊢ τ ~ ⋆
-  (** factorization through ground types *)
-  | consTauStar τ τG :
-      UB (length A) τ ->
-      (Ground τ -> False) ->
-      (not (τ = ⋆)) →
-      (get_shape τ = Some τG) ->
-      A ⊢ τ ~ τG ->
-      A ⊢ τ ~ ⋆
-  | consStarTau τ τG :
-      UB (length A) τ ->
-      (Ground τ -> False) ->
-      (not (τ = ⋆)) →
-      (get_shape τ = Some τG) ->
-      A ⊢ τG ~ τ ->
-      A ⊢ ⋆ ~ τ
-  (** identiy casts between Base and ⋆ *)
-  | consBaseBase :
-      A ⊢ TUnit ~ TUnit
-  | consStarStar :
-      A ⊢ ⋆ ~ ⋆
-  (** between types of same structure *)
-  (* between + types *)
-  | consTSumTSum τ1 τ1' τ2 τ2' :
-      A ⊢ τ1 ~ τ1' ->
-      A ⊢ τ2 ~ τ2' ->
-      A ⊢ (τ1 + τ2)%type ~ (τ1' + τ2')%type
-  (* between × types *)
-  | consTProdTProd τ1 τ1' τ2 τ2' :
-      A ⊢ τ1 ~ τ1' ->
-      A ⊢ τ2 ~ τ2' ->
-      A ⊢ (τ1 × τ2) ~ (τ1' × τ2')
-  (* between → types *)
-  | consTArrowTArrow τ1 τ2 τ3 τ4 :
-      A ⊢ τ3 ~ τ1 ->
-      A ⊢ τ2 ~ τ4 ->
-      A ⊢ (TArrow τ1 τ2) ~ (TArrow τ3 τ4)
-  (** exposing recursive calls *)
-  (* between μ types; we always *expose* a recursive call here *)
-  | consTRecTRecNoStars τl τr (Pl : not_star τl) (Pr : not_star τr) :
-      (NoStars NotYet τl τr Pl Pr :: A) ⊢ τl ~ τr -> (* we add the assumption to our list (the two type bodies) *)
-      (* Sometimes, this recursive call we not be useful though (e.g. μ.(TUnit + N × 0) ~ μ.(TUnit + N × ⋆)) *)
-      (* In these cases, we need to expose an eXtra recursive call. *)
-      (* See consTVarStar and consStarTVar *)
-      A ⊢ (TRec τl) ~ (TRec τr)
-  | consTRecTRecStarOnLeft τr :
-      (StarOnLeft τr :: A) ⊢ ⋆ ~ τr -> (* we add the assumption to our list *)
-      (* We will always be able to use the recursive call *)
-      A ⊢ (TRec ⋆) ~ (TRec τr)
-  | consTRecTRecStarOnRight τl :
-      (StarOnRight τl :: A) ⊢ τl ~ ⋆ -> (* we add the assumption to our list *)
-      (* We will always be able to use the recursive call *)
-      A ⊢ (TRec τl) ~ (TRec ⋆)
-  (* exposing new recursive call because previous one was not usable *)
-  | consTVarStar i τl τr Pl Pr :
-      (A !! i) = Some (NoStars NotYet τl τr Pl Pr) ->
-      (update A i (NoStars ForStarOnRight τl τr Pl Pr)) ⊢ τl ~ ⋆ ->
-      A ⊢ (TVar i) ~ (TRec ⋆)
-  | consStarTVar i τl τr Pl Pr :
-      (A !! i) = Some (NoStars NotYet τl τr Pl Pr) ->
-      (update A i (NoStars ForStarOnLeft τl τr Pl Pr)) ⊢ ⋆ ~ τr ->
-      A ⊢ (TRec ⋆) ~ (TVar i)
-  (** using previously exposed recursive calls *)
-  (* *using* previously exposed recursion ; two variables *)
-  | consTVars i τl τr Pl Pr :
-      (A !! i) = Some (NoStars NotYet τl τr Pl Pr) -> (* we need this specific information when proving well-typedness !! *)
-      A ⊢ (TVar i) ~ (TVar i)
-  (* using recurion; one variable, one star *)
-  | consTVarStarUse i τl :
-      (A !! i) = Some (StarOnRight τl) ->
-      A ⊢ (TVar i) ~ (TRec ⋆)
-  | consStarTVarUse i τr :
-      (A !! i) = Some (StarOnLeft τr) ->
-      A ⊢ (TRec ⋆) ~ (TVar i)
-  | consTVarStarUseX i τl τr Pl Pr :
-      (A !! i) = Some (NoStars ForStarOnRight τl τr Pl Pr) ->
-      A ⊢ (TVar i) ~ (TRec ⋆)
-  | consStarTVarUseX i τl τr Pl Pr :
-      (A !! i) = Some (NoStars ForStarOnLeft τl τr Pl Pr) ->
-      A ⊢ (TRec ⋆) ~ (TVar i)
-where "A ⊢ τ ~ τ'" := (cons_struct A τ τ').
+(* contains lot's of errors still; *)
+(* trying to fix these will make it even more complex... *)
+(* let's try with unfolding of the types... instead *)
 
-(* UBA A implies that A is appropriately bounded *)
-Inductive UBA : list Assumption -> Type :=
-  | emptyUBA :
-      UBA []
-  | consUBANostars A F τl τr Pl Pr (pUBl : UB (S (length A)) τl) (pUBr : UB (S (length A)) τr) (pUBA : UBA A) :
-      UBA (NoStars F τl τr Pl Pr :: A)
-  | consUBAStarOnLeft A τl (pUBl : UB (S (length A)) τl) (pUBA : UBA A) :
-      UBA (StarOnLeft τl :: A)
-  | consUBAStarOnRight A τr (pUBr : UB (S (length A)) τr) (pUBA : UBA A) :
-      UBA (StarOnLeft τr :: A).
+(* Reserved Notation "A ⊢ τ ~ τ'" (at level 4, τ, τ' at next level). *)
+(* Inductive cons_struct (A : list Assumption) : type -> type -> Type := *)
+(*   (** ground and star *) *)
+(*   (* downcasting from star to ground *) *)
+(*   | consStarTGround τ : *)
+(*       Ground τ → *)
+(*       A ⊢ ⋆ ~ τ *)
+(*   (* upcasting from ground to star *) *)
+(*   | consTGroundStar τ : *)
+(*       Ground τ → *)
+(*       A ⊢ τ ~ ⋆ *)
+(*   (** factorization through ground types *) *)
+(*   | consTauStar τ τG : *)
+(*       UB (length A) τ -> *)
+(*       (Ground τ -> False) -> *)
+(*       (not (τ = ⋆)) → *)
+(*       (get_shape τ = Some τG) -> *)
+(*       A ⊢ τ ~ τG -> *)
+(*       A ⊢ τ ~ ⋆ *)
+(*   | consStarTau τ τG : *)
+(*       UB (length A) τ -> *)
+(*       (Ground τ -> False) -> *)
+(*       (not (τ = ⋆)) → *)
+(*       (get_shape τ = Some τG) -> *)
+(*       A ⊢ τG ~ τ -> *)
+(*       A ⊢ ⋆ ~ τ *)
+(*   (** identiy casts between Base and ⋆ *) *)
+(*   | consBaseBase : *)
+(*       A ⊢ TUnit ~ TUnit *)
+(*   | consStarStar : *)
+(*       A ⊢ ⋆ ~ ⋆ *)
+(*   (** between types of same structure *) *)
+(*   (* between + types *) *)
+(*   | consTSumTSum τ1 τ1' τ2 τ2' : *)
+(*       A ⊢ τ1 ~ τ1' -> *)
+(*       A ⊢ τ2 ~ τ2' -> *)
+(*       A ⊢ (τ1 + τ2)%type ~ (τ1' + τ2')%type *)
+(*   (* between × types *) *)
+(*   | consTProdTProd τ1 τ1' τ2 τ2' : *)
+(*       A ⊢ τ1 ~ τ1' -> *)
+(*       A ⊢ τ2 ~ τ2' -> *)
+(*       A ⊢ (τ1 × τ2) ~ (τ1' × τ2') *)
+(*   (* between → types *) *)
+(*   | consTArrowTArrow τ1 τ2 τ3 τ4 : *)
+(*       A ⊢ τ3 ~ τ1 -> *)
+(*       A ⊢ τ2 ~ τ4 -> *)
+(*       A ⊢ (TArrow τ1 τ2) ~ (TArrow τ3 τ4) *)
+(*   (** exposing recursive calls *) *)
+(*   (* between μ types; we always *expose* a recursive call here *) *)
+(*   | consTRecTRecNoStars τl τr (Pl : not_star τl) (Pr : not_star τr) : *)
+(*       (NoStars NotYet τl τr Pl Pr :: A) ⊢ τl ~ τr -> (* we add the assumption to our list (the two type bodies) *) *)
+(*       (* Sometimes, this recursive call we not be useful though (e.g. μ.(TUnit + N × 0) ~ μ.(TUnit + N × ⋆)) *) *)
+(*       (* In these cases, we need to expose an eXtra recursive call. *) *)
+(*       (* See consTVarStar and consStarTVar *) *)
+(*       A ⊢ (TRec τl) ~ (TRec τr) *)
+(*   | consTRecTRecStarOnLeft τr : *)
+(*       (StarOnLeft τr :: A) ⊢ ⋆ ~ τr -> (* we add the assumption to our list *) *)
+(*       (* We will always be able to use the recursive call *) *)
+(*       A ⊢ (TRec ⋆) ~ (TRec τr) *)
+(*   | consTRecTRecStarOnRight τl : *)
+(*       (StarOnRight τl :: A) ⊢ τl ~ ⋆ -> (* we add the assumption to our list *) *)
+(*       (* We will always be able to use the recursive call *) *)
+(*       A ⊢ (TRec τl) ~ (TRec ⋆) *)
+(*   (* exposing new recursive call because previous one was not usable *) *)
+(*   | consTVarStar i τl τr Pl Pr : *)
+(*       (A !! i) = Some (NoStars NotYet τl τr Pl Pr) -> *)
+(*       (update A i (NoStars ForStarOnRight τl τr Pl Pr)) ⊢ τl ~ ⋆ -> *)
+(*       A ⊢ (TVar i) ~ (TRec ⋆) *)
+(*   | consStarTVar i τl τr Pl Pr : *)
+(*       (A !! i) = Some (NoStars NotYet τl τr Pl Pr) -> *)
+(*       (update A i (NoStars ForStarOnLeft τl τr Pl Pr)) ⊢ ⋆ ~ τr -> *)
+(*       A ⊢ (TRec ⋆) ~ (TVar i) *)
+(*   (** using previously exposed recursive calls *) *)
+(*   (* *using* previously exposed recursion ; two variables *) *)
+(*   | consTVars i τl τr Pl Pr : *)
+(*       (A !! i) = Some (NoStars NotYet τl τr Pl Pr) -> (* we need this specific information when proving well-typedness !! *) *)
+(*       A ⊢ (TVar i) ~ (TVar i) *)
+(*   (* using recurion; one variable, one star *) *)
+(*   | consTVarStarUse i τl : *)
+(*       (A !! i) = Some (StarOnRight τl) -> *)
+(*       A ⊢ (TVar i) ~ (TRec ⋆) *)
+(*   | consStarTVarUse i τr : *)
+(*       (A !! i) = Some (StarOnLeft τr) -> *)
+(*       A ⊢ (TRec ⋆) ~ (TVar i) *)
+(*   | consTVarStarUseX i τl τr Pl Pr : *)
+(*       (A !! i) = Some (NoStars ForStarOnRight τl τr Pl Pr) -> *)
+(*       A ⊢ (TVar i) ~ (TRec ⋆) *)
+(*   | consStarTVarUseX i τl τr Pl Pr : *)
+(*       (A !! i) = Some (NoStars ForStarOnLeft τl τr Pl Pr) -> *)
+(*       A ⊢ (TRec ⋆) ~ (TVar i) *)
+(* where "A ⊢ τ ~ τ'" := (cons_struct A τ τ'). *)
 
 (** Notes *) (*
 In the end, we are only concerned with [] ⊢ τ ~ τ' (τ and τ' closed).
