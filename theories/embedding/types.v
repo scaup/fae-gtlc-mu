70fb7f9e686723439b67573d0cc055cbebb77f47
Koen Jacobs <koen.pub@pm.me>
2020-04-09 16:03:25 +0200

    embedding types

---
+++ theories/embedding/types.v
@@ -0,0 +1,13 @@
+From fae_gtlc_mu Require Import stlc_mu.typing.
+From fae_gtlc_mu Require Import cast_calculus.types.
+
+Reserved Notation "[| τ |]" (at level 4, τ at next level).
+Fixpoint embed_type (τ : stlc_mu.typing.type) : type :=
+  match τ with
+  | typing.TUnit => TUnit
+  | typing.TProd τ1 τ2 => TProd [| τ1 |] [| τ2 |]
+  | typing.TSum τ1 τ2 => TSum [| τ1 |] [| τ2 |]
+  | typing.TArrow τ1 τ2 => TArrow [| τ1 |] [| τ2 |]
+  | typing.TRec τb => TRec [| τb |]
+  | typing.TVar x => TVar x
+  end where "[| e |]" := (embed_type e).
